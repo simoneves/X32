20f9017e2ca5ac19c887e4cc1b466b39dcdfaf24
Simon Eves <github.com@eves.us>
2017-04-27 14:19:58 -0700

    v1.4 Now generates data for use in Cue mode Import the .shw file Allows non-integer cue numbers

--- X32Snippets.py
+++ X32Snippets.py
@@ -5,7 +5,7 @@
 # X32 Snippets
 #
 # Last mod:
-# January 23, 2017
+# April 27, 2017
 #
 # Written by:
 # Simon Eves (simon@eves.us)
@@ -13,12 +13,13 @@
 # Additional contributions by:
 # Craig Flint (craig@cliveflint.co.uk)
 # Aaron Yoffe (aaron@blownfuse.org)
+# Art Zemon (art@zemon.name)
 #
 # Free for non-commercial use
 #
 ################################################################################
 
-VERSION = "1.3.1"
+VERSION = "1.4"
 
 ################################################################################
 # Imports
@@ -38,14 +39,14 @@ from pyexcel_ods import get_data
 
 SHEET_NAME      = "Sheet1" # spreadsheet sub-sheet name containing all data
 
-SKIP_ROWS       = 4      # number of spreadsheet rows to skip before extracting data
+SKIP_ROWS       = 5      # number of spreadsheet rows to skip before extracting data
 
-SNIPPET_COL     = 2      # spreadsheet column of snippet index data (must be monotonically incrementing integers)
-CUE_COL         = 3      # spreadsheet column of cue data (any short string)
+CUE_NUM_COL     = 0      # spreadsheet column of snippet index data (must be monotonically incrementing integers)
+CUE_LABEL_COL   = 1      # spreadsheet column of cue data (any short string)
 
 FIRST_CHAN      = 1      # number of first physical channel to control
 FIRST_CHAN_COL  = 4      # spreadsheet column of data for that first channel
-NUM_CHANS       = 14     # number of contiguous set of channels
+NUM_CHANS       = 21     # number of contiguous set of channels
 
 FIRST_BUS       = 0      # number of first physical bus to control (13 = FX1)
 FIRST_BUS_COL   = 0      # spreadsheet column of data for that first bus
@@ -55,18 +56,18 @@ FIRST_AUXIN     = 0      # number of first physical auxin to control (5 = AuxIn 
 FIRST_AUXIN_COL = 0      # spreadsheet column of data for that first auxin
 NUM_AUXINS      = 0      # number of contiguous set of auxins (2 = AuxIn 5-6)
 
-FIRST_DCA_COL   = 19     # spreadsheet column of data for first DCA
+FIRST_DCA_COL   = 26     # spreadsheet column of data for first DCA
 NUM_DCAS        = 8      # number of DCAs
+DCA_COLOR       = 'WH'   # color for active DCA labels
 
 # Craig Flint functionality
-WARN_DCAS       = False  # show next cue's active DCAs in red?
+CF_WARN_DCAS    = False  # show next cue's active DCAs in red?
+CF_WARN_COLOR   = 'RD'   # color for warning DCA labels (if WARN_DCAS)
 
-# STC Urinetown functionality
-UT_FX           = False  # negative path DCA indexes mean also un-mute that path's FX send to the bus below
-UT_FX_BUS       = '14'   # set the send from the paths to this FX bus (zero-padded number as string)
+# STC functionality
+STC_FX          = False  # negative path DCA indexes mean also un-mute that path's FX send to the bus below
+STC_FX_BUS      = '14'   # set the send from the paths to this FX bus (zero-padded number as string)
 
-DCA_COLOR       = 'WH'   # color for active DCA labels
-WARN_COLOR      = 'RD'   # color for warning DCA labels (if WARN_DCAS)
 
 ################################################################################
 # Functions
@@ -82,7 +83,7 @@ def ods_cell(d, r, c):
         #print "DEBUG: cell " + str(r) + "/" + str(c) + " is type " + str(type(cell))
         if type(cell) == unicode:
             cell = cell.encode('utf-8')
-        return cell
+        return str(cell)
     except:
         return ''
 
@@ -93,7 +94,7 @@ def process_paths(ods, snp_file, row_index, first_path, first_path_col, num_path
     # first we find which paths in this cue have any DCA assignment and therefore need to be unmuted
     mutes = []
     for path in range(0, num_paths):
-        dca = str(ods_cell(ods, row_index, path + first_path_col))
+        dca = ods_cell(ods, row_index, path + first_path_col)
         if dca != '':
             mutes.append(False)
         else:
@@ -106,7 +107,7 @@ def process_paths(ods, snp_file, row_index, first_path, first_path_col, num_path
 
     # then we write out the new DCA assignments
     for path in range(0, num_paths):
-        dca = str(ods_cell(ods, row_index, path + first_path_col))
+        dca = ods_cell(ods, row_index, path + first_path_col)
         bitmap = 0
         if dca != '':
             bitmap = 1 << (abs(int(float(dca))) - 1)
@@ -119,19 +120,19 @@ def process_paths(ods, snp_file, row_index, first_path, first_path_col, num_path
         if not mutes[path]:
             snp_file.write('/' + osc_prefix + '/' + str(path + first_path).zfill(2) + '/mix/on ON\n')
 			
-    # Urinetown
+    # STC
     # for channels only, also control the mute of the given FX bus send for this path
-    if UT_FX and path_type_is_channel:
+    if STC_FX and path_type_is_channel:
         for path in range(0, num_paths):
-            dca = str(ods_cell(ods, row_index, path + first_path_col))
+            dca = ods_cell(ods, row_index, path + first_path_col)
             fx_on = False
             if dca != '':
                 fx_on = int(float(dca)) < 0
             if fx_on:
                 print "DEBUG: activating FX send on channel " + str(path + first_path)
-                snp_file.write('/' + osc_prefix + '/' + str(path + first_path).zfill(2) + '/mix/' + UT_FX_BUS + ' ON\n')
+                snp_file.write('/' + osc_prefix + '/' + str(path + first_path).zfill(2) + '/mix/' + STC_FX_BUS + ' ON\n')
             else:
-                snp_file.write('/' + osc_prefix + '/' + str(path + first_path).zfill(2) + '/mix/' + UT_FX_BUS + ' OFF\n')
+                snp_file.write('/' + osc_prefix + '/' + str(path + first_path).zfill(2) + '/mix/' + STC_FX_BUS + ' OFF\n')
 
 
 ################################################################################
@@ -164,32 +165,48 @@ if __name__ == "__main__":
     # process file
     #
     
+    # report
+    print "Opening spreadsheet..."
+    
     # read the file
     ods = get_data(ods_file_name)[SHEET_NAME]
     
     # init these
     row_index = SKIP_ROWS
     snp_index = 0
+    cue_numbers = []
+    cue_labels = []
+    
+    # report
+    print "Creating cues..."
     
     # iterate rows until the end
     for row_index in range(SKIP_ROWS, len(ods)):
-
-        # get snippet
-        snippet = ods_cell(ods, row_index, SNIPPET_COL)
-        if type(snippet) == float:
-            snippet = str(int(snippet))
-        else:
-            snippet = str(snippet)
         
-        # skip rows with no snippet
-        if snippet == '':
+        # get cue
+        cue = ods_cell(ods, row_index, CUE_NUM_COL)
+        
+        # skip rows with no cue
+        if cue == '':
             continue
             
-        # get cue
-        cue = str(ods_cell(ods, row_index, CUE_COL))
+        # the end?
+        if cue == "END":
+            break
+            
+        # get cue number
+        cue_number = ''
+        try:
+            cue_number = str(int(float(cue) * 100.0))
+        except:
+            print 'ERROR: Found invalid cue number at row ' + str(row_index + 1)
+            sys.exit()
+            
+        # get cue label
+        cue_label = ods_cell(ods, row_index, CUE_LABEL_COL)
         
         # report snippet
-        print "Generating new Snippet '" + snippet + "', Cue '" + cue + "'"
+        print 'Generating new cue "' + cue + '", label "' + cue_label + '"'
             
         # open snippet file
         snp_file = open(show_name + '.' + str(snp_index).zfill(3) + '.snp', 'w')
@@ -199,36 +216,71 @@ if __name__ == "__main__":
         
         # process channels
         process_paths(ods, snp_file, row_index, FIRST_CHAN, FIRST_CHAN_COL, NUM_CHANS, 'ch', True)
-
+        
         # process buses
         process_paths(ods, snp_file, row_index, FIRST_BUS, FIRST_BUS_COL, NUM_BUSES, 'bus', False)
-
+        
         # process auxins
         process_paths(ods, snp_file, row_index, FIRST_AUXIN, FIRST_AUXIN_COL, NUM_AUXINS, 'auxin', False)
-
+        
         # finally we write out the new DCA labels
         for dca in range(0, NUM_DCAS):
-            label = str(ods_cell(ods, row_index, dca + FIRST_DCA_COL))
-            labelbelow = str(ods_cell(ods, row_index + 1, dca + FIRST_DCA_COL))
+            label = ods_cell(ods, row_index, dca + FIRST_DCA_COL)
+            labelbelow = ods_cell(ods, row_index + 1, dca + FIRST_DCA_COL)
             if label != '':
                 snp_file.write('/dca/' + str(dca + 1) + '/config/name "' + label + '"\n')
                 snp_file.write('/dca/' + str(dca + 1) + '/config/color ' + DCA_COLOR + '\n')
-            elif WARN_DCAS and labelbelow != '':
+            elif CF_WARN_DCAS and labelbelow != '':
                 snp_file.write('/dca/' + str(dca + 1) + '/config/name "' + labelbelow + '"\n')
-                snp_file.write('/dca/' + str(dca + 1) + '/config/color ' + WARN_COLOR + '\n')
+                snp_file.write('/dca/' + str(dca + 1) + '/config/color ' + CF_WARN_COLOR + '\n')
             else:
                 snp_file.write('/dca/' + str(dca + 1) + '/config/name ""\n')
                 snp_file.write('/dca/' + str(dca + 1) + '/config/color OFF\n')
-
-        # close file
+        
+        # close snippet file
         snp_file.close()
-
-        # the end?
-        if snippet == "END":
-            break
-            
+        
+        # store
+        cue_numbers.append(cue_number)
+        cue_labels.append(cue_label)
+        
         # next
         snp_index = snp_index + 1
+    
+    #
+    # generate show file
+    #
+    
+    # report
+    print "Creating show file..."
+    
+    # open show file
+    shw_file = open(show_name + '.shw', 'w')
+    
+    # start show
+    shw_file.write('#2.6#\n')
+    shw_file.write('show "' + show_name +'" 0 0 0 0 0 0 0 0 0 0 "X32-Edit 3.00"\n')
+    
+    # report
+    print "Writing cues..."
+    
+    # write cues
+    cue_index = 0
+    for cue_number in cue_numbers:
+        shw_file.write('cue/' + str(cue_index).zfill(3) + ' ' + cue_number + ' "' + cue_labels[cue_index] + '" 0 -1 ' + str(cue_index) + ' 0 1 0 0\n')
+        cue_index = cue_index + 1
+    
+    # report
+    print "Writing snippets..."
+    
+    # write snippet refs
+    snp_index = 0
+    for cue_number in cue_numbers:
+        shw_file.write('snippet/' + str(snp_index).zfill(3) + ' "' + cue_labels[snp_index] + '" 0 0 0 0 1\n')
+        snp_index = snp_index + 1
+        
+    # close show file
+    shw_file.close()
     
     # all done
     print "Goodbye!"
