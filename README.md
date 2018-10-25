80b5d9c0e31c19d25f664c5102ae5741e8806b50
Simon Eves <simon.eves@mapd.com>
2018-10-25 09:18:42 -0700

    v1.9 Updated README Renamed some control parameters NOTE: SPREADSHEET ROW AND COLUMN REFERENCES NOW 1-BASED!

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # X32Snippets
 
-Current Version 1.8
+Current Version 1.9
 
-August 30, 2018
+October 25, 2018
 
 ### Background
 
@@ -26,18 +26,18 @@ The files here are the Python script itself, a Bash script to run it with a cert
 
 The spreadsheet has to be in a certain format, and you have to edit the Python script to tell it which parts of the spreadsheet to look at.
 
-NOTE! All row and column indexes described below start from ZERO (Column A = Index 0, Row 1 = Index 0)
+NOTE: Row numbers are as in the spreadsheet (starting from 1). Column numbers are also 1-based (A = 1, B = 2, Z = 26, AA = 27 etc.)
 
 ### Control Parameters
 
 **SHEET_NAME**
-This is the default name of an OpenOffice spreadsheet document, as displayed in the tabs at the bottom. Change this string if you've changed the name
+This is the default name of an OpenOffice spreadsheet document, as displayed in the tabs at the bottom. Change this string if you've changed the name of the sheet which contains the DCA data. Note that this does not have to be the first sheet, and other sheets will be ignored.
 
 **SKIP_ROWS**
-How many rows from the top to skip before attempting to interpret the spreadsheet contents
+How many rows from the top to skip before attempting to interpret the spreadsheet contents.
 
 **CUE_NUM_COL / CUE_LABEL_COL**
-The column numbers of the columns from which to pull the cue number and the cue label string
+The column numbers of the columns from which to pull the cue number and the cue label string.
 
 **PATH_NUM_ROW**
 The row containing the physical path (channel/bus/aux-in) number of each logical path. This data is mandatory. By default just enter a 1:1 mapping (starting with 1), but if your channels are not a contiguous block then you can use this to skip gaps or reorder them.
@@ -61,25 +61,26 @@ The number of DCAs to generate data for (must be 8 or less)
 **DCA_COLOR**
 The desired backlight color for the DCA labels (when active) (can be `RD`, `GN`, `YE`, `BL`, `MG`, `CY`, `WH`, `RDi`, `GNi`, `YEi`, `BLi`, `MGi`, `CYi`, `WHi`)
 
+**NAME_CHANS**
+Enabling this feature will cause the channel labels to be updated as well as the DCA labels. This requires an additional set of columns in the spreadsheet, starting at **FIRST_CHAN_NAME_COL**. If a cell in those columns is non-empty, the channel name will be changed to that value on that cue.
+
+**DCA_ALT_LABEL_COLORS**
+Enabling this feature will cause DCA labels which match any of the strings in **DCA_ALT_LABELS** to be colored as **DCA_ALT_LABEL_COLOR** instead of **DCA_COLOR**.
+
+**DCA_ACTIVE_ON_NEXT_CUE**
+Enabling this feature will cause DCA faders which will be active on the *next* cue to be illuminated with the color specified in **DCA_ACTIVE_ON_NEXT_CUE_COLOR** (value options same as above) to indicate that they might be allowed to stay up. (NOTE: this code only checks the next spreadsheet row, so will not detect the next active DCA if there are row gaps between cues, and is mutually exclusive with DCA_SAME_ON_NEXT_CUE)
+
+**DCA_SAME_ON_NEXT_CUE**
+Enabling this feature will cause DCA faders which will be active on the *next* cue *with the same contents* to be illuminated with the color specified in **DCA_SAME_ON_NEXT_CUE_COLOR** (value options same as above) to indicate that they might be allowed to stay up. (that this code only checks the next spreadsheet row, so will not detect the next active DCA if there are row gaps between cues, and is mutually exclusive with DCA_ACTIVE_ON_NEXT_CUE)
+
+**FX_UNMUTE**
+Enabling this feature will additionally control the bus send mute for any given channel to the single FX bus specified by **FX_UNMUTE_BUS**. If the DCA number in the channel column is *negative*, the channel will still be assigned to the DCA of the positive value, but the bus send for that channel will also be un-muted. This allows, for example, reverb to be selectively applied to channels in a given cue, and not in others.
+
+**OTHER_MUTES**
+Enabling this feature will additionally control the mutes of another contiguous range of channels (perhaps the band channels) defined by **OTHER_MUTES_FIRST_CHAN** and **OTHER_MUTES_NUM_CHANS**. One of more of those channels can be muted on any given cue by entering that channel number in any of the spreadsheet columns defined by the array **OTHER_MUTES_COLS**. For example, if OTHER_MUTES_FIRST_CHAN is set to 17, OTHER_MUTES_NUM_CHANS is set to 11, and OTHER_MUTES_COLS is set to [31, 32, 33], then entering a channel number between 17 and 27 into any of columns 31-33 will cause that channel to be muted on that cue, for example, if you know that acoustic instrument will not be played during that cue (to mute handling noise).
+
 ### Usage
 
 Download these files, edit the ./runit.sh script appropriately, make it executable (this script will only work on Mac or Linux), and run it.
 
-NOTE: The resulting files have only been tested to load successfully on a console running firmware v3.07.
-
-### Craig Flint functionality
-
-Enabling **CF_WARN_DCAS** will cause DCA faders which will be active on the *next* cue to be illuminated with the color specified in **CF_WARN_COLOR** (value options same as above). Note that this code only checks the next spreadsheet row, so will not detect the next active DCA if there are row gaps between cues.
-
-Enabling **CF_NAME_CHANS** will cause the channel labels to be updated as well as the DCA labels. This requires an additional set of columns in the spreadsheet, starting at **CF_FIRST_CHAN_NAME_COL**. If a cell in those columns is non-empty, the channel name will be changed to that value on that cue.
-
-Enabling **CF_ALT_LABEL_COLORS** will cause DCA labels which match any of the strings in **CF_ALT_LABELS** to be colored as **CF_ALT_COLOR** instead of **DCA_COLOR**.
-
-### STC functionality
-
-Enabling **STC_FX** will additionally control the bus send mute for any given channel to the single FX bus specified by **STC_FX_BUS**. If the DCA number in the channel column is negative, the channel will still be assigned to the DCA of the positive value, but the bus send for that channel will be un-muted. This allows, for example, reverb to be selectively applied to channels in a given cue, and not in others.
-
-(document **SE_BAND_MUTES**)
-
-Enabling **AY_DCA_SAME** (and setting a color with **AY_DCA_SAME_COLOR**) will cause DCA labels which do not change on the next cue to be lit in that different color, so as to indicate to the user that those faders can be left up through the next cue change.
-
+NOTE: The resulting files have only been tested to load successfully on a console running firmware 3.07 and later.
--- X32Snippets.py
+++ X32Snippets.py
@@ -5,7 +5,7 @@
 # X32 Snippets
 #
 # Last mod:
-# August 30, 2018
+# October 25, 2018
 #
 # Written by:
 # Simon Eves (simon@eves.us)
@@ -19,7 +19,7 @@
 #
 ################################################################################
 
-VERSION = "1.8"
+VERSION = "1.9"
 
 ################################################################################
 # Imports
@@ -37,74 +37,71 @@ from pyexcel_ods import get_data
 # Set these appropriately for your source spreadsheet layout
 ################################################################################
 
-# values for Heathers
+SHEET_NAME                 = 'Sheet1' # spreadsheet sub-sheet name containing all data
 
-SHEET_NAME             = 'Sheet1' # spreadsheet sub-sheet name containing all data
+SKIP_ROWS                  = 3     # number of spreadsheet rows to skip before extracting data
 
-SKIP_ROWS              = 3      # number of spreadsheet rows to skip before extracting data
+CUE_NUM_COL                = 1     # spreadsheet column of cue number data (can be integer or tenths, e.g. "1" or "1.1")
+CUE_LABEL_COL              = 2     # spreadsheet column of cue data (any short string)
 
-CUE_NUM_COL            = 0      # spreadsheet column of cue number data (can be integer or tenths, e.g. "1" or "1.1")
-CUE_LABEL_COL          = 1      # spreadsheet column of cue data (any short string)
+PATH_NUM_ROW               = 3     # row containing path (chan, bus, aux) numbers
 
-PATH_NUM_ROW           = 2      # row containing path (chan, bus, aux) numbers
+FIRST_CHAN_COL             = 5     # spreadsheet column of data for that first channel
+NUM_CHANS                  = 17    # number of contiguous set of channels
 
-FIRST_CHAN_COL         = 4      # spreadsheet column of data for that first channel
-NUM_CHANS              = 17     # number of contiguous set of channels
+FIRST_BUS_COL              = 0     # spreadsheet column of data for that first bus
+NUM_BUSES                  = 0     # number of contiguous set of buses (4 = FX1-4)
 
-FIRST_BUS_COL          = 0      # spreadsheet column of data for that first bus
-NUM_BUSES              = 0      # number of contiguous set of buses (4 = FX1-4)
+FIRST_AUXIN_COL            = 0     # spreadsheet column of data for that first auxin
+NUM_AUXINS                 = 0     # number of contiguous set of auxins (2 = AuxIn 5-6)
 
-FIRST_AUXIN_COL        = 0      # spreadsheet column of data for that first auxin
-NUM_AUXINS             = 0      # number of contiguous set of auxins (2 = AuxIn 5-6)
+FIRST_DCA_COL              = 23    # spreadsheet column of data for first DCA
+NUM_DCAS                   = 7     # number of DCAs
+DCA_COLOR                  = 'WH'  # color for active DCA labels
 
-FIRST_DCA_COL          = 22     # spreadsheet column of data for first DCA
-NUM_DCAS               = 7      # number of DCAs
-DCA_COLOR              = 'WH'   # color for active DCA labels
+NAME_CHANS                 = False # set channel names
+FIRST_CHAN_NAME_COL        = 0     # spreadsheet column of name for the first channel
 
-# Craig Flint warn upcoming DCAs
-CF_WARN_DCAS           = True  # show next cue's active DCAs
-CF_WARN_COLOR          = 'RD'   # color for warning DCA labels (if WARN_DCAS)
+DCA_ALT_LABEL_COLORS       = False
+DCA_ALT_LABELS             = [ 'Reverb', 'Sh Dly', 'Lg Dly', 'Delay' ] # color these DCA labels differently
+DCA_ALT_LABEL_COLOR        = 'MG'
 
-# Craig Flint name channels
-CF_NAME_CHANS          = False # set channel names
-CF_FIRST_CHAN_NAME_COL = 0     # spreadsheet column of name for the first channel
+DCA_ACTIVE_ON_NEXT_CUE     = False # alternative color for DCAs that are still active on the next cue
+DCA_ACTIVE_ON_NEXT_CUE_COLOR = 'RD'  # that color
 
-# Craig Flint alternative color DCA labels
-CF_ALT_LABEL_COLORS    = False
-CF_ALT_LABELS          = [ 'Reverb', 'Sh Dly', 'Lg Dly', 'Delay' ] # color these DCA labels differently
-CF_ALT_COLOR           = 'MG'
+DCA_SAME_ON_NEXT_CUE       = False # alternative color for DCAs that stay the same on the next cue
+DCA_SAME_ON_NEXT_CUE_COLOR = 'GN'  # that color
 
-# Simon Eves FX send automation
-SE_FX                  = False  # negative path DCA indexes mean also un-mute that path's FX send to the bus below
-SE_FX_BUS              = '14'   # set the send from the paths to this FX bus (zero-padded number as string)
+FX_UNMUTE                  = False # negative path DCA indexes mean also un-mute that path's FX send to the bus below
+FX_UNMUTE_BUS              = 13    # set the send from the paths to this single FX bus
 
-# Simon Eves band muting automation
-SE_BAND_MUTES          = True   # mute one or more band channel per cue, all others will be unmuted (e.g. for switching basses)
-SE_FIRST_BAND_CHAN     = 17     # first board channel for the band
-SE_NUM_BAND_CHANS      = 11     # number of consecutive board channels for the band
-SE_BAND_MUTE_COLS      = [ 31, 32, 33 ] # spreadsheet column of data for first channel to mute
-
-# Aaron Yoffe DCA-stays-the-same
-AY_DCA_SAME            = True
-AY_DCA_SAME_COLOR      = 'GN'
+OTHER_MUTES                = False # mute one or more band channel per cue, all others will be unmuted (e.g. for switching basses)
+OTHER_MUTES_FIRST_CHAN     = 17    # first board channel for the band
+OTHER_MUTES_NUM_CHANS      = 8     # number of consecutive board channels for the band
+OTHER_MUTES_COLS           = [ ]   # spreadsheet column of data for first channel to mute
 
 ################################################################################
 # Functions
 ################################################################################
 
-def ods_cell(d, r, c):
+def read_cell_as_string(d, r, c):
 
     # robust function to pull contents of a spreadsheet cell as a string, regardless of actual contents
+    # incoming indices are 1-based
 
     try:
-        row = d[r]
-        cell = row[c]
+        row = d[r - 1]
+        cell = row[c - 1]
         #print "DEBUG: cell " + str(r) + "/" + str(c) + " is type " + str(type(cell))
         if type(cell) == unicode:
             cell = cell.encode('utf-8')
         return str(cell)
     except:
         return ''
+
+def string_to_int(s):
+    
+    return int(float(s))
 
 def process_paths(ods, snp_file, row_index, first_path_col, num_paths, osc_prefix):
 
@@ -114,7 +111,7 @@ def process_paths(ods, snp_file, row_index, first_path_col, num_paths, osc_prefi
     # first we find which paths in this cue have any DCA assignment and therefore need to be unmuted
     mutes = []
     for path in range(0, num_paths):
-        dca = ods_cell(ods, row_index, path + first_path_col)
+        dca = read_cell_as_string(ods, row_index, path + first_path_col)
         if dca != '':
             mutes.append(False)
         else:
@@ -123,24 +120,24 @@ def process_paths(ods, snp_file, row_index, first_path_col, num_paths, osc_prefi
     # then we write out mute-ons for paths which have become muted
     for path in range(0, num_paths):
         if mutes[path]:
-            path_num = int(float(ods_cell(ods, PATH_NUM_ROW, path + first_path_col)))
+            path_num = string_to_int(read_cell_as_string(ods, PATH_NUM_ROW, path + first_path_col))
             snp_file.write('/' + osc_prefix + '/' + str(path_num).zfill(2) + '/mix/on OFF\n')
 
     # then we write out the new DCA assignments
     for path in range(0, num_paths):
-        dca = ods_cell(ods, row_index, path + first_path_col)
+        dca = read_cell_as_string(ods, row_index, path + first_path_col)
         bitmap = 0
         if dca != '':
-            bitmap = 1 << (abs(int(float(dca))) - 1)
+            bitmap = 1 << (abs(string_to_int(dca)) - 1)
         else:
             bitmap = 0
-        path_num = int(float(ods_cell(ods, PATH_NUM_ROW, path + first_path_col)))
+        path_num = string_to_int(read_cell_as_string(ods, PATH_NUM_ROW, path + first_path_col))
         snp_file.write('/' + osc_prefix + '/' + str(path_num).zfill(2) + '/grp/dca ' + str(bitmap) + '\n')
     
     # then we write out mute-offs for paths which have become un-muted
     for path in range(0, num_paths):
         if not mutes[path]:
-            path_num = int(float(ods_cell(ods, PATH_NUM_ROW, path + first_path_col)))
+            path_num = string_to_int(read_cell_as_string(ods, PATH_NUM_ROW, path + first_path_col))
             snp_file.write('/' + osc_prefix + '/' + str(path_num).zfill(2) + '/mix/on ON\n')
             
 def next_dca_label(ods, row_index, col):
@@ -148,23 +145,23 @@ def next_dca_label(ods, row_index, col):
     # function to find the contents of the cell in col 'col' in the next row below 'row_index' that has a valid cue number
 
     # are we already at the END?
-    search_cue = ods_cell(ods, row_index, CUE_NUM_COL)
+    search_cue = read_cell_as_string(ods, row_index, CUE_NUM_COL)
     if search_cue == 'END':
         return ''
 
     # find the next row that has a valid cue
     search_row = row_index + 1
-    search_cue = ods_cell(ods, search_row, CUE_NUM_COL)
+    search_cue = read_cell_as_string(ods, search_row, CUE_NUM_COL)
     while search_cue == '':
         search_row = search_row + 1
-        search_cue = ods_cell(ods, search_row, CUE_NUM_COL)
+        search_cue = read_cell_as_string(ods, search_row, CUE_NUM_COL)
 
     # was it the END
     if search_cue == 'END':
         return ''
 
     # otherwise return whatever's in the column
-    return ods_cell(ods, search_row, col)
+    return read_cell_as_string(ods, search_row, col)
 
 def current_or_previous_channel_name(ods, row_index, col):
 
@@ -176,10 +173,10 @@ def current_or_previous_channel_name(ods, row_index, col):
 
     # search upwards to find the previous non-empty cell in this column
     search_row = row_index
-    search_name = ods_cell(ods, search_row, col)
+    search_name = read_cell_as_string(ods, search_row, col)
     while search_name == '' and search_row > SKIP_ROWS:
         search_row = search_row - 1
-        search_name = ods_cell(ods, search_row, col)
+        search_name = read_cell_as_string(ods, search_row, col)
 
     # return
     return search_name
@@ -235,7 +232,7 @@ if __name__ == "__main__":
     for row_index in range(SKIP_ROWS, len(ods)):
         
         # get cue
-        cue = ods_cell(ods, row_index, CUE_NUM_COL)
+        cue = read_cell_as_string(ods, row_index, CUE_NUM_COL)
         
         # skip rows with no cue
         if cue == '':
@@ -248,13 +245,13 @@ if __name__ == "__main__":
         # get cue number
         cue_number = ''
         try:
-            cue_number = str(int(float(cue) * 100.0))
+            cue_number = str(string_to_int(cue) * 100.0)
         except:
             print 'ERROR: Found invalid cue number at row ' + str(row_index + 1)
             sys.exit()
             
         # get cue label
-        cue_label = ods_cell(ods, row_index, CUE_LABEL_COL)
+        cue_label = read_cell_as_string(ods, row_index, CUE_LABEL_COL)
         
         # report snippet
         print 'Generating new cue "' + cue + '", label "' + cue_label + '"'
@@ -277,63 +274,57 @@ if __name__ == "__main__":
         if NUM_AUXINS > 0:
             process_paths(ods, snp_file, row_index, FIRST_AUXIN_COL, NUM_AUXINS, 'auxin')
         
-        # Simon Eves
         # for channels only, also control the mute of the given FX bus send for this path
-        if SE_FX:
+        if FX_UNMUTE:
             for chan in range(0, NUM_CHANS):
-                dca = ods_cell(ods, row_index, chan + FIRST_CHAN_COL)
+                dca = read_cell_as_string(ods, row_index, chan + FIRST_CHAN_COL)
                 fx_on = False
                 if dca != '':
-                    fx_on = int(float(dca)) < 0
-                chan_num = int(float(ods_cell(ods, PATH_NUM_ROW, chan + FIRST_CHAN_COL)))
+                    fx_on = string_to_int(dca) < 0
+                chan_num = string_to_int(read_cell_as_string(ods, PATH_NUM_ROW, chan + FIRST_CHAN_COL))
                 if fx_on:
-                    print 'DEBUG: activating FX send on channel ' + str(chan_num)
-                    snp_file.write('/ch/' + str(chan_num).zfill(2) + '/mix/' + SE_FX_BUS + ' ON\n')
+                    snp_file.write('/ch/' + str(chan_num).zfill(2) + '/mix/' + str(FX_UNMUTE_BUS).zfill(2) + ' ON\n')
                 else:
-                    snp_file.write('/ch/' + str(chan_num).zfill(2) + '/mix/' + SE_FX_BUS + ' OFF\n')
+                    snp_file.write('/ch/' + str(chan_num).zfill(2) + '/mix/' + str(FX_UNMUTE_BUS).zfill(2) + ' OFF\n')
         
-        # Craig Flint
         # for channels only, set name from additional spreadsheet data
-        if CF_NAME_CHANS:
+        if NAME_CHANS:
             for chan in range(0, NUM_CHANS):
-                name_on_or_above = current_or_previous_channel_name(ods, row_index, chan + CF_FIRST_CHAN_NAME_COL)
-                chan_num = int(float(ods_cell(ods, PATH_NUM_ROW, chan + FIRST_CHAN_COL)))
+                name_on_or_above = current_or_previous_channel_name(ods, row_index, chan + FIRST_CHAN_NAME_COL)
+                chan_num = string_to_int(read_cell_as_string(ods, PATH_NUM_ROW, chan + FIRST_CHAN_COL))
                 if name_on_or_above != '':
-                    print 'DEBUG: renaming channel ' + str(chan_num) + ' as "' + name_on_or_above + '"'
                     snp_file.write('/ch/' + str(chan_num).zfill(2) + '/config/name "' + name_on_or_above + '"\n')
         
-        # Simon Eves
-        # mute specified band channels
-        if SE_BAND_MUTES:
-            for chan in range(0, SE_NUM_BAND_CHANS):
+        # mute specified channels in range
+        if OTHER_MUTES:
+            for chan in range(0, OTHER_MUTES_NUM_CHANS):
                 mute_this_chan = False
-                for col in SE_BAND_MUTE_COLS:
-                    mute_info = ods_cell(ods, row_index, col)
+                for col in OTHER_MUTES_COLS:
+                    mute_info = read_cell_as_string(ods, row_index, col)
                     if mute_info != '':
-                        mute_chan = int(float(mute_info))
-                        if mute_chan == chan + SE_FIRST_BAND_CHAN:
+                        mute_chan = string_to_int(mute_info)
+                        if mute_chan == chan + OTHER_MUTES_FIRST_CHAN:
                             mute_this_chan = True
                 if mute_this_chan:
-                    print 'DEBUG: muting band channel ' + str(chan + SE_FIRST_BAND_CHAN)
-                    snp_file.write('/ch/' + str(chan + SE_FIRST_BAND_CHAN).zfill(2) + '/mix/on OFF\n')
+                    snp_file.write('/ch/' + str(chan + OTHER_MUTES_FIRST_CHAN).zfill(2) + '/mix/on OFF\n')
                 else:
-                    snp_file.write('/ch/' + str(chan + SE_FIRST_BAND_CHAN).zfill(2) + '/mix/on ON\n')
+                    snp_file.write('/ch/' + str(chan + OTHER_MUTES_FIRST_CHAN).zfill(2) + '/mix/on ON\n')
         
         # finally we write out the new DCA labels
         for dca in range(0, NUM_DCAS):
-            label = ods_cell(ods, row_index, dca + FIRST_DCA_COL)
+            label = read_cell_as_string(ods, row_index, dca + FIRST_DCA_COL)
             next_label = next_dca_label(ods, row_index, dca + FIRST_DCA_COL)
             if label != '':
                 snp_file.write('/dca/' + str(dca + 1) + '/config/name "' + label + '"\n')
-                if CF_ALT_LABEL_COLORS and label in CF_ALT_LABELS:
-                    snp_file.write('/dca/' + str(dca + 1) + '/config/color ' + CF_ALT_COLOR + '\n')
-                elif AY_DCA_SAME and label == next_label:
-                    snp_file.write('/dca/' + str(dca + 1) + '/config/color ' + AY_DCA_SAME_COLOR + '\n')
+                if DCA_ALT_LABEL_COLORS and label in DCA_ALT_LABELS:
+                    snp_file.write('/dca/' + str(dca + 1) + '/config/color ' + DCA_ALT_LABEL_COLOR + '\n')
+                elif DCA_SAME_ON_NEXT_CUE and label == next_label:
+                    snp_file.write('/dca/' + str(dca + 1) + '/config/color ' + DCA_SAME_ON_NEXT_CUE_COLOR + '\n')
                 else:
                     snp_file.write('/dca/' + str(dca + 1) + '/config/color ' + DCA_COLOR + '\n')
-            elif CF_WARN_DCAS and next_label != '':
+            elif DCA_ACTIVE_ON_NEXT_CUE and next_label != '':
                 snp_file.write('/dca/' + str(dca + 1) + '/config/name "' + next_label + '"\n')
-                snp_file.write('/dca/' + str(dca + 1) + '/config/color ' + CF_WARN_COLOR + '\n')
+                snp_file.write('/dca/' + str(dca + 1) + '/config/color ' + DCA_ACTIVE_ON_NEXT_CUE_COLOR + '\n')
             else:
                 snp_file.write('/dca/' + str(dca + 1) + '/config/name ""\n')
                 snp_file.write('/dca/' + str(dca + 1) + '/config/color OFF\n')
